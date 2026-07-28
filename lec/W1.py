cc13cc12a16338b9a785dbe0635511a9ba779578
Keys <70819367+keys-i@users.noreply.github.com>
2026-07-28 13:29:30 +1000

    chore: format W1 code
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- lec/W1.py
+++ lec/W1.py
@@ -1,16 +1,17 @@
 """Code written during Lecture - W1"""
+
 from numpy import exp
 
 # variables
-x  = -0.4
+x = -0.4
 n = 10
 t = 1.0
 ssum = 1.0
 
-for i in range(1, n+1):
-    t *= x/i
+for i in range(1, n + 1):
+    t *= x / i
     ssum += t
-    print(f'i={i:3d} t={t:12.4e} ssum={ssum:16.8e}')
+    print(f"i={i:3d} t={t:12.4e} ssum={ssum:16.8e}")
 
-print("-"*50)
+print("-" * 50)
 print(f"the actual sum is exp({x:f})={exp(x):16.8e}")
