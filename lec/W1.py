63499f68fbd5e6a8dce07f61460d9230a2eaa843
Keys <70819367+keys-i@users.noreply.github.com>
2026-07-28 13:24:25 +1000

    chore: add lecture codes and tools
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

---
+++ lec/W1.py
@@ -0,0 +1,16 @@
+"""Code written during Lecture - W1"""
+from numpy import exp
+
+// variables
+x  = -0.4
+n = 10
+t = 1.0
+ssum = 1.0
+
+for i in range(1, n+1):
+    t *= x/i
+    ssum += t
+    print(f'i={i:3d} t={t:12.4e} ssum={ssum:16.8e}')
+
+print("-"*50)
+print(f"                  exp({x:f})={exp(x):16.8e}")
