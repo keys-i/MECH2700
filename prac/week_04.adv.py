177cf41d8cc4318961883efb196b3f9107e98b31
Keys <70819367+keys-i@users.noreply.github.com>
2026-08-14 18:11:13 +1000

    optimised implementation of week 4 codes
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- prac/week_04.adv.py
+++ prac/week_04.adv.py
@@ -1,3 +1,31 @@
 """
 3-point Gaussian quadrature between two points
 """
+
+import numpy as np
+
+# -- values --
+MU = 0.75
+SIGMA = 0.5
+
+XI = np.array([-np.sqrt(3 / 5), 0.0, np.sqrt(3 / 5)])
+W = np.array([5 / 9, 8 / 9, 5 / 9])
+
+
+# -- functs --
+def gaussian_quadrature_3pt(x0, x1):
+    midpt, half_width = (x0 + x1) / 2, (x1 - x0) / 2
+    x = midpt + half_width * XI
+
+    # Normal prob density
+    prob_density = np.exp(-0.5 * ((x - MU) / SIGMA) ** 2) / (
+        SIGMA * np.sqrt(2 * np.pi)
+    )
+
+    # 3-point Gaussian quadrature
+    return half_width * np.dot(W, prob_density)
+
+
+p = gaussian_quadrature_3pt(0.5, 1.0)
+
+print(f"3-point Gaussian quadrature estimate: {p:.10f}")
--- prac/week_04.py
+++ prac/week_04.py
@@ -1,3 +1,31 @@
 """
 3-point Gaussian quadrature between two points
 """
+
+import numpy as np
+
+# -- values --
+MU = 0.75
+SIGMA = 0.5
+
+XI = np.array([-np.sqrt(3 / 5), 0.0, np.sqrt(3 / 5)])
+W = np.array([5 / 9, 8 / 9, 5 / 9])
+
+
+# -- functs --
+def gaussian_quadrature_3pt(x0, x1):
+    x = (x0 + x1) / 2 + (x1 - x0) / 2 * XI
+
+    # Normal prob density
+    prob_density = (
+        1
+        / np.sqrt(2 * np.pi * SIGMA**2)
+        * np.exp(-((x - MU) ** 2) / (2 * SIGMA**2))
+    )
+
+    # 3-point Gaussian quadrature
+    return (x1 - x0) / 2 * np.sum(W * prob_density)
+
+p = gaussian_quadrature_3pt(0.5, 1.0)
+
+print(f"3-point Gaussian quadrature estimate: {p:.10f}")
