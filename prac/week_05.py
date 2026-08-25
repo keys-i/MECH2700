d15d3da7b84579b3aa46268d3b64e9fb56ac285c
Keys <70819367+keys-i@users.noreply.github.com>
2026-08-25 16:05:23 +1000

    added week 5 python codes
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

---
+++ prac/week_05.py
@@ -0,0 +1,38 @@
+"""
+Hydrocarbon-combustion Linear system using numpy
+"""
+
+import numpy as np
+
+
+def hydrocarbon_combustion():
+    A = np.array(
+        [
+            [2, -1, 0],
+            [8, -1, -2],
+            [0, 4, -1],
+        ],
+        dtype=float,
+    )
+    b = np.array([4, 0, 0], dtype=float)
+
+    return A, b
+
+
+def my_det(A):
+    return np.linalg.det(A)
+
+
+def determine_coefficients(A, b):
+    return np.linalg.solve(A, b)
+
+
+A, b = hydrocarbon_combustion()
+x = determine_coefficients(A, b)
+
+print(f"""
+A = {A}
+b = {b}
+det(A) = {my_det(A):g}
+x = {x}
+""")
