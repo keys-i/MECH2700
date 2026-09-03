c24be5ce02115df304e107dca8265d6320614113
Keys <70819367+keys-i@users.noreply.github.com>
2026-09-03 11:09:47 +1000

    finished codes for week 6
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- prac/week_06.py
+++ prac/week_06.py
@@ -4,21 +4,43 @@ Jacobi iteration for the temperature distribution in a heated support beam
 
 import numpy as np
 
+# -- consts --
+SOURCE_TERM = 4e5 * 0.1**2 / 170
+L_OFFSET, M_OFFSET, R_OFFSET = (
+    (300 + SOURCE_TERM) / 2,
+    SOURCE_TERM / 2,
+    (750 + SOURCE_TERM) / 2,
+)
+
 
 # -- functs --
 def update_temperatures(Ts: np.ndarray) -> np.ndarray:
-    pass
+    t0, t1, t2 = Ts
+    return np.array(
+        (L_OFFSET + 0.5 * t1, M_OFFSET + 0.5 * (t0 + t2), R_OFFSET + 0.5 * t1)
+    )
 
 
 def jacobi_iteration(
-    inital_guess: np.ndarray,
+    initial_guess: np.ndarray,
     n_iters: int,
 ):
-    pass
+    t0, t1, t2 = initial_guess
+
+    for _ in range(n_iters):
+        t0, t1, t2 = (
+            L_OFFSET + 0.5 * t1,
+            M_OFFSET + 0.5 * (t0 + t2),
+            R_OFFSET + 0.5 * t1,
+        )
+
+    return np.array((t0, t1, t2))
 
 
 def main():
-    pass
+    inital_guess = np.array((337.5, 375.0, 412.5), dtype=np.float64)
+    return jacobi_iteration(inital_guess, 5)
 
 
-# -- main --
+if __name__ == "__main__":
+    print(main())
