32a0e57a1a9ae751a605c3ce0a4b5c66ddfef090
Keys <70819367+keys-i@users.noreply.github.com>
2026-09-03 10:45:37 +1000

    added template for week 6
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

---
+++ prac/week_06.py
@@ -0,0 +1,24 @@
+"""
+Jacobi iteration for the temperature distribution in a heated support beam
+"""
+
+import numpy as np
+
+
+# -- functs --
+def update_temperatures(Ts: np.ndarray) -> np.ndarray:
+    pass
+
+
+def jacobi_iteration(
+    inital_guess: np.ndarray,
+    n_iters: int,
+):
+    pass
+
+
+def main():
+    pass
+
+
+# -- main --
